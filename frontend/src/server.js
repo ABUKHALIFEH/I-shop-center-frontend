bb6a0a02d1fbdf71d181005e923e334d0622a624
ABUKHALIFEH <120542539+ABUKHALIFEH@users.noreply.github.com>
2024-06-20 20:27:51 +0300

    ABU KHALIFEH

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000/api/v2";
+export const server =
+  "https://i-shop-center-project-qtja0c38p-abukhalifehs-projects.vercel.app/api/v2";
+export const backend_url =
+  "https://i-shop-center-project-qtja0c38p-abukhalifehs-projects.vercel.app/";
